00b5c04f5b3838554ea6a596d071688bd6a8aa88
depressedpsychiatrist <realatalatal@gmail.com>
2023-02-20 21:05:25 -0500

    available moves for pawn

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,9 +1,39 @@
 class Pawn < Piece
 
-  def move_dirs
-    [[0,1]]
+  def forward_dir
+    color == :black ? -1 : 1
+  end
+  
+  def starting_pos
+    color == :black ? 6 : 1
   end
 
+  def available_moves
+    moves = []
+    current_r, current_c = location
+
+    one_forward = [current_r + forward_dir, current_c]
+    if board.empty?(one_forward) 
+      moves << one_forward
+    end
+
+    two_forward = [current_r + (forward_dir * 2), current_c]
+    if current_r == starting_pos && board.empty?(one_forward) && board.empty?(two_forward)
+      moves << two_forward
+    end
+
+    diag_left = [current_r + forward_dir, current_c + 1]
+    diag_right = [current_r + forward_dir, current_c - 1]
+    if(enemy?(diag_left))
+      moves << diag_left
+    end
+    if enemy?(diag_right)
+      moves << diag_right
+    end
+    moves
+
+  end
+  
   def to_s
     color == :black ? "♙" : "♟"
   end
