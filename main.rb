c95ab843c1017e3e7a949ddb2cf01732968d4b5b
depressedpsychiatrist <realatalatal@gmail.com>
2023-02-20 21:05:35 -0500

    testing

--- main.rb
+++ main.rb
@@ -1,11 +1,11 @@
 require_relative "./lib/board.rb"
 require_relative "./lib/board_renderer_text.rb"
-b = Board.new
-b[[0,1]] = Rook.new(b, [0,1], :black)
-b[[0,0]] = Queen.new(b, [0,0], :black)
+# b = Board.new
+# b[[0,1]] = Rook.new(b, [0,1], :black)
+# b[[0,0]] = Queen.new(b, [0,0], :black)
 # b[[6,6]] = King.new(b, [6,6], :white)
+b = Board.start_chess
 text_board = BoardRendererText.new(b)
+b[[2,3]] = Pawn.new(b, [2,3], :white)
 puts text_board.render
-
-p b[[0,0]].available_moves.size
-p b[[0,0]].available_moves
+p b[[2,3]].available_moves
