40ad23924b5d40c8189f272236a15482a2ca7dc4
depressedpsychiatrist <realatalatal@gmail.com>
2023-02-20 21:05:11 -0500

    update start_chess include board and location

--- lib/board.rb
+++ lib/board.rb
@@ -5,24 +5,24 @@ class Board
   def self.start_chess
     board = self.new
     8.times do |c|
-      board[[1,c]] = Pawn.new(:white)
-      board[[6,c]] = Pawn.new(:black)
+      board[[1,c]] = Pawn.new(board, [1, c], :white)
+      board[[6,c]] = Pawn.new(board, [6, c], :black)
     end
     
     [[0, :white], [7, :black]].each do |(r, color)|
-      board[[r, 0]] = Rook.new(color)
-      board[[r, 7]] = Rook.new(color)
-      board[[r, 1]] = Knight.new(color)
-      board[[r, 6]] = Knight.new(color)
-      board[[r, 2]] = Bishop.new(color)
-      board[[r, 5]] = Bishop.new(color)
+      board[[r, 0]] = Rook.new(board, [r, 0], color)
+      board[[r, 7]] = Rook.new(board, [r, 7], color)
+      board[[r, 1]] = Knight.new(board, [r, 1], color)
+      board[[r, 6]] = Knight.new(board, [r, 6], color)
+      board[[r, 2]] = Bishop.new(board, [r, 2], color)
+      board[[r, 5]] = Bishop.new(board, [r, 5], color)
     end
     
-    board[[0, 3]] = King.new(:white)
-    board[[7, 3]] = King.new(:black)
+    board[[0, 3]] = King.new(board, [0, 3], :white)
+    board[[7, 3]] = King.new(board, [7, 3], :black)
 
-    board[[0, 4]] = Queen.new(:white)
-    board[[7, 4]] = Queen.new(:black)
+    board[[0, 4]] = Queen.new(board, [0, 4], :white)
+    board[[7, 4]] = Queen.new(board, [7, 4], :black)
 
     board
   end
